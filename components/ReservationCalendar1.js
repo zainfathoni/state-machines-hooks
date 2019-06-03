aaffb225ec249ba4c90c495c553c089ee1e6c2f6
Zain Fathoni <zain.fathoni@gmail.com>
2019-06-03 12:47:57 +0800

    Simplify ReservationCalendar component

--- components/ReservationCalendar1.js
+++ components/ReservationCalendar1.js
@@ -1,37 +1,14 @@
-import { Badge, Calendar } from 'antd'
+import { Calendar } from 'antd'
 import React from 'react'
-import '../app/ReservationCalendar.css'
-import { getStatusText, useDateMap } from '../app/utils'
+import { dataCellRender, useDateMap } from '../app/utils'
 
-export const ReservationCalendar1 = () => {
-  const { getDateData, getMonthData } = useDateMap()
-
-  const dateCellRender = date => (
-    <ul className="events">
-      {date &&
-        getDateData(date).map(({ status, time }) => (
-          <li key={time}>
-            <Badge status={status} text={`${time} ${getStatusText(status)}`} />
-          </li>
-        ))}
-    </ul>
-  )
-
-  const monthCellRender = date => {
-    const num = getMonthData(date)
-    return num ? (
-      <div className="notes-month">
-        <section>{num}</section>
-        <span>Pickups Completed</span>
-      </div>
-    ) : null
-  }
+export default () => {
+  const { getData } = useDateMap()
 
   return (
-    <main class="main-container">
+    <main className="container">
       <Calendar
-        dateCellRender={dateCellRender}
-        monthCellRender={monthCellRender}
+        dateCellRender={date => date && dataCellRender(getData(date))}
       />
     </main>
   )
